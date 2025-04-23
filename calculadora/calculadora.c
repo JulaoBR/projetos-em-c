35b1b60f8f3d77b014b26c63eeb366a76ef8a85a
Julio <julioscp2009@hotmail.com>
2025-04-23 00:32:12 +0000

    Criado menu interativo

--- calculadora/calculadora.c
+++ calculadora/calculadora.c
@@ -3,43 +3,56 @@
 int main() {
 
 	float num1, num2, resultado;
+	int opcao;
 
-	char operador;
+	do {
 
-	printf("Digite o primeiro numero: ");
-	scanf("%f", &num1);
+		printf("\n==== CALCULADORA ====\n");
+		printf("1 - Adição\n");
+		printf("2 - Subtração\n");
+		printf("3 - Multiplicação\n");
+		printf("4 - Divisão\n");
+		printf("0 - Sair\n");
+		printf("Escolha uma opção: ");
+		scanf("%d", &opcao);
 
-	printf("Digite o segundo numero: ");
-	scanf("%f", &num2);
-
-	printf("Digite o operador (+, -, *, /): ");
-	scanf(" %c", &operador);
-
-	switch(operador) {
-		case '+':
-			resultado = num1 + num2;
+		if (opcao == 0) {
+			printf("Saindo...\n");
 			break;
-		case '-':
-			resultado = num1 - num2;
-			break;
-		case '*':
-			resultado = num1 * num2;
-			break;
-		case '/':
-			if (num2 != 0) {
-				resultado = num1 / num2;
-			} else {
-				printf("Erro: divisao por zero nao permitida.\n");
-				return 1;
-			}
-			break;
-		default:
-			printf("Operador invalido!");
-			return 1;
-	}
+		}
 
-	printf("Resultado: %.2f\n", resultado);
+		printf("Digite o primeiro numero: ");
+		scanf("%f", &num1);
+
+		printf("Digite o segundo numero: ");
+		scanf("%f", &num2);
+
+		switch(opcao) {
+			case 1:
+				resultado = num1 + num2;
+				break;
+			case 2:
+				resultado = num1 - num2;
+				break;
+			case 3:
+				resultado = num1 * num2;
+				break;
+			case 4:
+				if (num2 != 0) {
+					resultado = num1 / num2;
+				} else {
+					printf("Erro: divisao por zero nao permitida.\n");
+				}
+				break;
+			default:
+				printf("Operador invalido!\n");
+		}
+
+		if (resultado > 0) {
+			printf("Resultado: %.2f\n", resultado);
+		}
+
+	} while (1);
 
 	return 0;
-
 }
